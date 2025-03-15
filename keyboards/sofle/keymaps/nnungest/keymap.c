980ebd7df2e7a36e1959ce9c8f3f5f8a7a6b8f3e
Nick Nungester <nnungest@gmail.com>
2025-03-15 12:44:56 -0400

    fix: raise layer commands

--- keyboards/sofle/keymaps/nnungest/keymap.c
+++ keyboards/sofle/keymaps/nnungest/keymap.c
@@ -108,8 +108,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                              KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,KC_TRNS,
   KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
-  KC_TRNS, KC_LALT,  KC_LCTL,  KC_LSFT,  KC_NO, KC_CAPS,                           KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT,  KC_DASH, KC_BSPC,
-  KC_TRNS, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_NO,  KC_TRNS,           KC_TRNS,  KC_NO, KC_LSTRT, KC_NO, KC_LEND,   KC_NO, KC_TRNS,
+  KC_TRNS, KC_ESC, KC_0,    KC_EQL,  KC_GRV,  LALT_T(KC_X),                          KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT,  KC_DASH, KC_BSPC,
+  KC_TRNS, C(KC_Z), C(KC_X), C(KC_C), LSG(KC_4), KC_TAB,  KC_TRNS,           KC_TRNS,  KC_NO, KC_LSTRT, KC_NO, KC_LEND,   KC_NO, KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, _______
 ),
 /* ADJUST
@@ -150,8 +150,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_CONTROLLER] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_TAB,   KC_Q,   KC_UP,    KC_ENTER,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
+  KC_ESC,   KC_LEFT,   KC_DOWN,    KC_RIGHT,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_NO,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  TL_COLEMAK,KC_LALT,KC_LCTL, KC_LSFT, KC_SPC,      KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
 )
