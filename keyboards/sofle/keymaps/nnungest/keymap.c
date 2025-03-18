fe37385bbc77b8c1dca62b17deaa08034cd8fa04
Nick Nungester <nnungest@gmail.com>
2025-03-18 01:01:39 -0400

    fix: allow rgb color setting

--- keyboards/sofle/keymaps/nnungest/keymap.c
+++ keyboards/sofle/keymaps/nnungest/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_NO, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-  TO(_CONTROLLER),  KC_LALT,  KC_LCTL, KC_LSFT, KC_SPC,                KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
+  TO(_CONTROLLER),  MO(_ADJUST),  KC_LCTL, KC_LSFT, KC_SPC,                KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
 ),
 /*
  * CONTROLLER / GAMING
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_UP,   KC_ENTER, KC_R, KC_T,                     KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC,
   KC_ESC,   KC_LEFT, KC_DOWN, KC_RIGHT, KC_F, KC_G,                     KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V, KC_B, KC_MUTE,     KC_NO, KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,
-          TO(_COLEMAK), KC_LALT, KC_LCTL, KC_LSFT,  KC_SPC,      KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
+          TO(_COLEMAK), MO(_ADJUST), KC_LCTL, KC_LSFT,  KC_SPC,      KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
 ),
 /*
  * LOWER
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT(
   KC_NO , KC_NO, KC_NO,     KC_NO,      KC_NO,   KC_NO,                     KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
   KC_NO , KC_NO, KC_QWERTY, KC_COLEMAK, KC_NO,   KC_NO,                     KC_NO, KC_LPRN, KC_RPRN, KC_RBRC, KC_NO, KC_NO,
-  KC_NO , KC_NO, KC_NO,     RM_HUEU,    KC_NO,   KC_NO,                     KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO,
+  KC_NO , KC_NO, KC_NO,     RGB_HUI,    KC_NO,   KC_NO,                     KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO,
   KC_NO , KC_NO, KC_NO,     KC_NO,      KC_NO,   KC_NO, KC_NO,       KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO,
                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
